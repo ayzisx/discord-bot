2b1cbc13025c69dd7053222dff4e808e4c4caa7d
ayzisx <eulerc626@gmail.com>
2026-05-06 11:56:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-const { Client, GatewayIntentBits, PermissionsBitField } = require("discord.js");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-
-const TOKEN = process.env.TOKEN;
-
-client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
-  // LOCK
-  if (message.content === "!lock") {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
-      return message.reply("You don't have permission.");
-    }
+  if (message.content === ",lock") {
+    if (!message.member.permissions.has("ManageChannels")) return;
 
     await message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {
       SendMessages: false,
     });
 
-    message.channel.send("🔒 Channel locked.");
+    message.channel.send("🔒 Locked");
   }
 
-  // UNLOCK
-  if (message.content === "!unlock") {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
-      return message.reply("You don't have permission.");
-    }
+  if (message.content === ",unlock") {
+    if (!message.member.permissions.has("ManageChannels")) return;
 
     await message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {
       SendMessages: true,
     });
 
-    message.channel.send("🔓 Channel unlocked.");
+    message.channel.send("🔓 Unlocked");
   }
 });
-
-client.login(TOKEN);
