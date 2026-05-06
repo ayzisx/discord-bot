3bd3eb79703b197277825a930e1ff983e9e63b5e
ayzisx <eulerc626@gmail.com>
2026-05-06 12:02:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
@@ -21,3 +35,5 @@ client.on("messageCreate", async (message) => {
     message.channel.send("🔓 Unlocked");
   }
 });
+
+client.login(process.env.TOKEN);
